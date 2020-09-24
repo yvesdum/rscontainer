c19e55004fd195d55930ba67b44b18ee15020712
yvesd99 <yvd-dev@outlook.com>
2020-09-24 20:50:29 +0200

    Expand docs

--- src/service.rs
+++ src/service.rs
@@ -1,16 +1,32 @@
 //! The traits to be implemented on foreign types by users of this crate.
 
-use crate::ServiceContainer;
 use crate::pointer::IPointer;
+use crate::ServiceContainer;
 
 /// An object that can be constructed through the service container.
 ///
 /// Implement this trait on a type to be able to use it as a static service.
 ///
+/// # Using foreign types as services
+///
+/// To use a foreign type as a service, follow these steps:
+///
+/// 1. Create a Zero Sized Type, for example `enum DatabaseService {}`.
+/// 2. Implement `IService` on this type.
+/// 3. Set the `Instance` associated type to the foreign type.
+/// 4. Set the `Pointer` associated type to a smart pointer to the foreign
+///    type, for example `Arc<Mutex<Database>>`.
+/// 5. Implement `construct()` and `construct_singleton()` to construct the
+///    foreign type.
+/// 6. When you want to resolve the service, you use `Singleton<>` with your
+///    Zero Sized Type, and you will get a pointer to the foreign type.
+///
 /// # Examples
 ///
+/// Primary use case:
+///
 /// ```
-/// use rscontainer::{ServiceContainer, IService};
+/// use rscontainer::{ServiceContainer, IService, Singleton};
 /// use std::rc::Rc;
 /// use std::cell::RefCell;
 ///
@@ -34,6 +50,37 @@ use crate::pointer::IPointer;
 ///         Rc::new(RefCell::new(Self::construct(ctn)))
 ///     }
 /// }
+///
+/// // Use as follows:
+/// let mut container = ServiceContainer::new();
+/// let account: Singleton<Account> = container.resolve();
+/// ```
+///
+/// Foreign services:
+///
+/// ```
+/// use rscontainer::{ServiceContainer, IService};
+/// use std::rc::Rc;
+/// use std::cell::RefCell;
+/// use diesel::pg::PgConnection;
+///
+/// enum PgConnectionService {}
+/// impl IService for PgConnectionService {
+///     type Instance = PgConnection;
+///     type Pointer = Rc<RefCell<PgConnection>>;
+///
+///     fn construct(ctn: &mut ServiceContainer) -> Self::Instance {
+///         PgConnection::establish()
+///     }
+///
+///     fn construct_singleton(ctn: &mut ServiceContainer) -> Self::Pointer {
+///         Rc::new(RefCell::new(Self::construct(ctn)))
+///     }
+/// }
+///
+/// // Use as follows:
+/// let mut container = ServiceContainer::new();
+/// let account: Singleton<PgConnectionService> = container.resolve();
 /// ```
 pub trait IService {
     /// The smart pointer to the service instance in case its a singleton.
@@ -48,7 +95,9 @@ pub trait IService {
 
     /// The type of the service in case its a non-shared instance.
     ///
-    /// Typically this would be `Self`.
+    /// Typically this would be `Self`. However, it is also possible to return
+    /// a completely different type. Therefore it is possible to use any
+    /// existing type as a service.
     type Instance;
 
     /// Creates an instance of this service.
@@ -60,7 +109,7 @@ pub trait IService {
 
 /// A trait object that can be used as a dynamic service.
 ///
-/// Implement this on the trait object itself with 
+/// Implement this on the trait object itself with
 /// `impl IDynService for dyn X {}`.
 pub trait IDynService {
     /// A ref counted smart pointer to this trait object.
